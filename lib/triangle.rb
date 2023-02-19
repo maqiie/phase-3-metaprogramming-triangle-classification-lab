62411448f502ff0c63bbf7c53d8110980e637a72
maqiie <99504010+maqiie@users.noreply.github.com>
2023-02-19 17:44:29 +0300

    completed the lab

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,30 @@
 class Triangle
-  # write code here
+  class TriangleError < StandardError; end
+
+  attr_reader :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+    validate_triangle
+  end
+
+  def kind
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  private
+
+  def validate_triangle
+    if [a, b, c].any? { |side| side <= 0 } || a + b <= c || b + c <= a || a + c <= b
+      raise TriangleError
+    end
+  end
 end
